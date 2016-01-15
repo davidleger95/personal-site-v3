82a9c213cdba8f59a65d68fb7c65f6afa9e5c21a
David Leger <davidleger95@me.com>
2016-01-14 21:55:39 -0400

    homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**************************************************
  *
- * Functions for [project name]
+ * Functions for Personal Website
  * AUTHOR   David Leger
  *          www.davidleger.me
  *
@@ -8,34 +8,64 @@
 
 $(document).ready(function(){
 
-    //bgColor();
     mobileNav();
-    randomQuote();
+    modal();
 });
-/*
-// Randomizes the background color excluding darker tones.
-function bgColor(){
-    var randColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    //console.log(randColor);
-    $('body').css('background', randColor);
-}
-*/
-function mobileNav(){
-    $('.menu-icon').on('click', function(){
-        $('.navbar-wrapper, .menu-icon span').toggleClass('nav-active');
-    });
-}
 
-// Grabs a random quote from quote.json
-function randomQuote(){
-    //alert("hi");
-    $.getJSON("/assets/js/quotes.json", function(result){
-        //alert(result);
-        var max = result.quotes.length;
-        var quoteIndex = Math.floor(Math.random()*max);
-        //console.log(quoteIndex);
-        
-        $('.quote').text('"' + result.quotes[quoteIndex].quote + '"');
-        $('.quote-credit').text('- ' + result.quotes[quoteIndex].person);
+// Mobile navigation view controls
+function mobileNav(){
+    $('.nav-menu-btn').on('click', function(){
+        $('.nav-menu-btn, .nav-menu').toggleClass('open');
     });
+} // mobileNav()
+
+// Modal view controls
+function modal(){
+    
+    // opens modal with href matching modal id
+    $('.modal-open-ctrl').on('click', function(){
+        
+        var id = $(this).attr('href');
+        $(id + ', .modal-splash').addClass('modal-open');
+        
+        return false;
+    });
+    
+    //closes all open modals
+    $('.modal-close-ctrl').on('click', function(){
+        $('.modal-open, .modal-splash').removeClass('modal-open');
+    });
+} // modal()
+
+// sends validates and sends contact form to Bev Bramble
+function contact(){
+    
+    var contact = $('#contact-form').serializeObject();
+    
+    var emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+    
+    if(contact.message == ""){
+        alert("ERROR: Please fill the message field.");
+        return false;
+    }
+    
+    if(!emailRegex.test(contact.email) && contact.email != ""){
+        alert("ERROR: Please enter a valid email address.");
+        return false;
+    }
+    
+    $.ajax({
+        url: "//formspree.io/davidleger95@me.com", 
+        method: "POST",
+        data: contact,
+        dataType: "json",
+        success: function(){
+            alert("SUCCESS! Message sent!");
+            $('#contact-form')[0].reset();
+            return false;
+        }
+    });
+    
+    
+    return false;
 }
